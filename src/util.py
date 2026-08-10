e669af05e0cd057364c9ecacd8c161b47f6669f6
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-08-10 23:58:11 +0530

    Modified util

--- src/util.py
+++ src/util.py
@@ -1,11 +1,16 @@
-from dataclasses import dataclass, fields
+from dataclasses import fields
+from typing import get_type_hints
+
 
 def validate(obj):
+    hints = get_type_hints(type(obj))
+
     for field in fields(obj):
         value = getattr(obj, field.name)
+        expected = hints[field.name]
 
-        if not isinstance(value, field.type):
+        if not isinstance(value, expected):
             raise TypeError(
-                f"{field.name} must be {field.type.__name__}, "
-                f"got {type(value).__name__}"
+                f"{field.name} must be "
+                f"{expected}, got {type(value)}"
             )
