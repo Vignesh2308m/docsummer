3422fae8c1af3a8441455a029b3c54ff8d11c96a
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-08-12 09:22:42 +0530

    Testing the flow

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from src.doc_parser import extract_page
-from ingestion_pipeline import RustDocStorage
-from storage_pipeline import RAGStorage
-from ast_parser import RustASTChunk, RustASTChunker
 from pathlib import Path
+import json
 
 
 
+from src.doc_parser import parse_html
+from src.ingestion import read_html
+from src.doc_builder import RustDocumentBuilder
+
+
+PATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html/alloc/borrow/trait.BorrowMut.html"
 
 def main():
-    doc_str = extract_page(Path("C://Users//Vickynila//.rustup//toolchains//stable-x86_64-pc-windows-msvc//share//doc//rust//html//alloc//borrow//trait.BorrowMut.html"))
-    print(doc_str)
+    html = read_html(Path(PATH))
+    doc = parse_html(html)
+    builder = RustDocumentBuilder()
+    rust_doc = builder.build(doc)
+    print(rust_doc)
+    pass
 
 
 if __name__ == '__main__':
