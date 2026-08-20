b4322254b9059c6ee9f80d441330082b777af6f4
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-08-20 23:20:26 +0530

    Modified main

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-from pathlib import Path
-import json
+from bs4 import BeautifulSoup, Tag
 
 
+PATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html/alloc/alloc/trait.Allocator.html"
 
-from src.doc_parser import parse_html
-from src.ingestion import read_html
-from src.doc_builder import RustDocumentBuilder
+def print_dom(element, depth=0):
+    indent = "  " * depth
 
+    if isinstance(element, Tag):
+        attrs = []
 
-PATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html/alloc/borrow/trait.BorrowMut.html"
+        if element.get("id"):
+            attrs.append(f'id="{element["id"]}"')
+
+        if element.get("class"):
+            attrs.append(f'class="{" ".join(element["class"])}"')
+
+        attr_text = " " + " ".join(attrs) if attrs else ""
+
+        print(f"{indent}<{element.name}{attr_text}>")
+
+        for child in element.children:
+            print_dom(child, depth + 1)
+
 
 def main():
-    html = read_html(Path(PATH))
-    doc = parse_html(html)
-    builder = RustDocumentBuilder()
-    rust_doc = builder.build(doc)
-    print(rust_doc)
-    pass
+    with open(PATH, encoding="utf-8") as f:
+        soup = BeautifulSoup(f, "lxml")
+
+    print_dom(soup.html)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
