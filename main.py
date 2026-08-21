ff434269a2a966926ee95272fffa746dceb9d144
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-08-21 07:05:48 +0530

    Changed Main

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 from bs4 import BeautifulSoup, Tag
+import json
 
+from src.parser import extract_html
+from src.templates.trait import TRAIT_TEMPLATE
 
-PATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html/alloc/alloc/trait.Allocator.html"
-
-def print_dom(element, depth=0):
-    indent = "  " * depth
-
-    if isinstance(element, Tag):
-        attrs = []
-
-        if element.get("id"):
-            attrs.append(f'id="{element["id"]}"')
-
-        if element.get("class"):
-            attrs.append(f'class="{" ".join(element["class"])}"')
-
-        attr_text = " " + " ".join(attrs) if attrs else ""
-
-        print(f"{indent}<{element.name}{attr_text}>")
-
-        for child in element.children:
-            print_dom(child, depth + 1)
+PATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html/alloc/str/pattern/trait.Pattern.html"
 
 
 def main():
     with open(PATH, encoding="utf-8") as f:
-        soup = BeautifulSoup(f, "lxml")
-
-    print_dom(soup.html)
-
+        result = extract_html(f,TRAIT_TEMPLATE)
+        with open("trait_test.json","w") as fp:
+            json.dump(result, fp, indent=4)
 
 if __name__ == "__main__":
     main()
