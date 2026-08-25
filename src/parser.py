26ff12de43656d815807b23a4a18c12adc126b7b
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-08-25 07:05:48 +0530

    parser done for trait

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-
+from models import Trait, TraitImplementor, TraitMethod
 
 def extract_html(html, config):
     soup = BeautifulSoup(html, "html.parser")
@@ -57,6 +57,45 @@ def extract_html(html, config):
 
     return result
 
+def trait_to_dataclasses(data: dict):
+    trait = Trait(
+        id=None,
+        name=data["name"],
+        type=data["type"],
+        declaration=data.get("declaration"),
+        description=data.get("description"),
+        source_href=data.get("source_href"),
+        source_line=data.get("source_line"),
+    )
+
+    methods = [
+        TraitMethod(
+            id=None,
+            trait_id=0,  # assigned after SQLite inserts Trait
+            name=method["name"],
+            kind=method["kind"],
+            href=method.get("href"),
+            signature=method.get("signature"),
+            description=method.get("description"),
+            source_href=method.get("source_href"),
+            source_line=method.get("source_line"),
+        )
+        for method in data.get("methods", [])
+    ]
+
+    implementors = [
+        TraitImplementor(
+            id=None,
+            trait_id=0,  # assigned after SQLite inserts Trait
+            name=implementor["name"],
+            href=implementor.get("href"),
+            source_href=implementor.get("source_href"),
+            source_line=implementor.get("source_line"),
+        )
+        for implementor in data.get("implementors", [])
+    ]
+
+    return trait, methods, implementors
 
 def coverage(html, document):
     soup = BeautifulSoup(html, "lxml")
