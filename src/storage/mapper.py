1006045510f2956c9b12bb58f3980c1c9a827c52
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-08-26 22:42:44 +0530

    changed mapper

--- src/storage/mapper.py
+++ src/storage/mapper.py
@@ -1,15 +1,15 @@
-from models import Trait, TraitImplementor, TraitMethod
+from .models import Trait, TraitImplementor, TraitMethod
 
 def trait_to_dataclasses(data: dict):
 
     trait = Trait(
         id=None,
-        name=data["name"],
+        name=data["title"],
         type=data["type"],
         declaration=data.get("declaration"),
         description=data.get("description"),
-        source_href=data.get("source_href"),
-        source_line=data.get("source_line"),
+        source_text=data.get("source", {}).get("text"),
+        source_href=data.get("source", {}).get("href"),
     )
 
     methods = [
@@ -21,8 +21,8 @@ def trait_to_dataclasses(data: dict):
             href=method.get("href"),
             signature=method.get("signature"),
             description=method.get("description"),
-            source_href=method.get("source_href"),
-            source_line=method.get("source_line"),
+            source_text=method.get("source", {}).get("text"),
+            source_href=method.get("source", {}).get("href"),
         )
         for method in data.get("methods", [])
     ]
@@ -33,8 +33,8 @@ def trait_to_dataclasses(data: dict):
             trait_id=0,  # assigned after SQLite inserts Trait
             name=implementor["name"],
             href=implementor.get("href"),
-            source_href=implementor.get("source_href"),
-            source_line=implementor.get("source_line"),
+            source_text=implementor.get("source", {}).get("text"),
+            source_href=implementor.get("source", {}).get("href"),
         )
         for implementor in data.get("implementors", [])
     ]
