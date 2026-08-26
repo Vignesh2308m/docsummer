f145a440d30e26f97d9f275f7c8dbb02da868abc
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-08-26 22:42:29 +0530

    added main

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-from bs4 import BeautifulSoup, Tag
-import json
+import sqlite3
 
-from src.parser import extract_html
+from src.storage.executor import create_table
+from src.storage.queries import TRAIT_TABLE, TRAIT_IMPLEMENTORS_TABLE, TRAIT_METHODS_TABLE
+
+
 from src.templates.trait import TRAIT_TEMPLATE
+from src.extraction.filesystem import lookup
+from src.extraction.parser import extract_html
+from src.storage.mapper import trait_to_dataclasses
 
-PATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html/alloc/str/pattern/trait.Pattern.html"
+DOCPATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html"
+DBPATH = "rust.db"
 
+def init():
+    with sqlite3.connect(DBPATH) as conn:
+        tbls = [
+            TRAIT_TABLE,
+            TRAIT_METHODS_TABLE,
+            TRAIT_IMPLEMENTORS_TABLE
+        ]
+        for tbl in tbls:
+            create_table(conn, tbl)
+
+def ingest():
+    pass
 
 def main():
-    with open(PATH, encoding="utf-8") as f:
-        result = extract_html(f,TRAIT_TEMPLATE)
-        with open("trait_test.json","w") as fp:
-            json.dump(result, fp, indent=4)
+    init()
+    trait_iter = lookup(DOCPATH, "trait.*.html")
+    for i in trait_iter:
+        with open(i,encoding="utf-8") as f:
+            html = extract_html(f, TRAIT_TEMPLATE)
+            trait, trait_method, trait_implementor = trait_to_dataclasses(html) 
+            print(trait)
+            print(trait_method)
+            print(trait_implementor)
+            break
+    pass
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
