b5f4ee0e4386582f44bb99aebbb5b216df81e3c9
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-09-03 22:53:32 +0530

    Modified queries

--- src/storage/queries.py
+++ src/storage/queries.py
@@ -1,7 +1,9 @@
 
 DOCUMENT_TABLE = '''
 CREATE TABLE IF NOT EXISTS documents (
-    id INTEGER PRIMARY KEY,
+    path TEXT,
+    page_id INTEGER,
+    id INTEGER,
     parent INTEGER NOT NULL DEFAULT 0,
     html_tag TEXT NOT NULL,
     html_id TEXT,
@@ -10,13 +12,31 @@ CREATE TABLE IF NOT EXISTS documents (
     content TEXT
 );
 '''
-
+RELATION_TABLE = '''
+CREATE TABLE IF NOT EXISTS relations (
+    path TEXT,
+    page_id INTEGER,
+    source_id INTEGER,
+    target_id INTEGER,
+    relation TEXT
+);
+'''
 
 DOCUMENT_INSERT = """
         INSERT INTO documents (
+            path, page_id,
             id, parent, html_tag,
             html_id, html_class,
             href, content
         )
-        VALUES (?, ?, ?, ?, ?, ?, ?)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+"""
+
+RELATION_INSERT = """
+        INSERT INTO documents (
+            path, page_id,
+            source_id, target_id,
+            relation
+        )
+        VALUES (?, ?, ?, ?, ?)
 """
