fb8bff57760322e4c6694d292f7956877fbbf733
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-09-03 22:52:34 +0530

    Changed main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.storage.executor import create_table, batch_insert
 from src.extraction.filesystem import lookup
 from src.extraction.parser import parse_html, save_document
 
-DOCPATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html"
+DOCPATH = "C:/Users/Vickynila/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html/std"
 DBPATH = "rust.db"
 
 def init():
@@ -15,15 +15,15 @@ def init():
 
 def main():
     init()
-    trait_iter = lookup(DOCPATH, "trait.*.html")
+    trait_iter = lookup(DOCPATH, "*.html")
+    count = 0
     for i in trait_iter:
+        print(str(count) + " " + str(i))
         with open(i,encoding="utf-8") as f:
-            html = parse_html(f, "main")
-            save_document(html, "sample.json")
-            
+            html = parse_html(f, str(i), count) 
             with sqlite3.connect("rust.db") as conn:
                 batch_insert(conn,DOCUMENT_INSERT,html)
-            break
+        count+=1
     pass
 
 if __name__ == '__main__':
