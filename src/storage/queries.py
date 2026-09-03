115e9350f3f592b8d95890ffe909fd5409c11f8b
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-09-03 17:15:29 +0530

    Modified queries

--- src/storage/queries.py
+++ src/storage/queries.py
@@ -1,86 +1,22 @@
 
-TRAIT_TABLE = """
-    CREATE TABLE IF NOT EXISTS traits (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        type TEXT NOT NULL,
-        declaration TEXT,
-        description TEXT,
-        source_text TEXT,
-        source_href TEXT
-    );
-"""
-
-TRAIT_METHODS_TABLE = """
-    CREATE TABLE IF NOT EXISTS trait_methods (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        trait_id INTEGER NOT NULL,
-        name TEXT NOT NULL,
-        kind TEXT NOT NULL CHECK (kind IN ('required', 'provided')),
-        href TEXT,
-        signature TEXT,
-        description TEXT,
-        source_text TEXT,
-        source_href TEXT,
-
-        FOREIGN KEY (trait_id)
-            REFERENCES traits(id)
-            ON DELETE CASCADE
-    );
-"""
+DOCUMENT_TABLE = '''
+CREATE TABLE IF NOT EXISTS documents (
+    id INTEGER PRIMARY KEY,
+    parent INTEGER NOT NULL DEFAULT 0,
+    html_tag TEXT NOT NULL,
+    html_id TEXT,
+    html_class TEXT,
+    href TEXT,
+    content TEXT
+);
+'''
 
 
-TRAIT_IMPLEMENTORS_TABLE = """
-    CREATE TABLE IF NOT EXISTS trait_implementors (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        trait_id INTEGER NOT NULL,
-        name TEXT NOT NULL,
-        href TEXT,
-        source_text TEXT,
-        source_href TEXT,
-
-        FOREIGN KEY (trait_id)
-            REFERENCES traits(id)
-            ON DELETE CASCADE
-    );
-"""
-
-
-TRAIT_INSERT = """
-    INSERT INTO traits (
-        title,
-        type,
-        declaration,
-        description,
-        source_text,
-        source_href
-    )
-    VALUES (?, ?, ?, ?, ?, ?)
-"""
-
-
-TRAIT_METHOD_INSERT = """
-    INSERT INTO trait_methods (
-        trait_id,
-        name,
-        kind,
-        href,
-        signature,
-        description,
-        source_text,
-        source_href
-    )
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-"""
-
-
-TRAIT_IMPLEMENTOR_INSERT = """
-    INSERT INTO trait_implementors (
-        trait_id,
-        name,
-        href,
-        source_text,
-        source_href
-    )
-    VALUES (?, ?, ?, ?, ?)
+DOCUMENT_INSERT = """
+        INSERT INTO documents (
+            id, parent, html_tag,
+            html_id, html_class,
+            href, content
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?)
 """
