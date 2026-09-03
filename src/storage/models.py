9d19d7f378adf99900ccb9271e2fad6401b81980
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-09-03 22:53:21 +0530

    Changed models

--- src/storage/models.py
+++ src/storage/models.py
@@ -6,6 +6,8 @@ from typing import Optional, List
 
 @dataclass
 class Document:
+    path: str
+    page_id: int
     id: int
     parent: int
     html_tag: str
@@ -13,4 +15,11 @@ class Document:
     html_class: str
     href: str
     content: str
-    
+
+@dataclass
+class Relations:
+    path: str
+    page_id: int
+    source_id: int
+    target_id: int
+    relation: str
