d04a9591097a9f67d6305916da859d018dd2326d
Vignesh Manoharan <vicky.manoharan2018@gmail.com>
2026-09-06 07:01:06 +0530

    Modified Parser

--- src/extraction/parser.py
+++ src/extraction/parser.py
@@ -1,9 +1,42 @@
 from bs4 import BeautifulSoup, Tag
 from bs4.element import NavigableString
-from src.storage.models import Document
-from dataclasses import asdict
+from dataclasses import asdict 
 import json
 
+from src.storage.models import Document, Relations
+
+
+
+def parse_relation(docs:list[Document], config: dict, field: str)->list[Relations]:
+    relations = []
+    for doc in docs:
+        col:str = getattr(doc, field)
+        rel = config["equals"].get(col) 
+        if rel:
+            relations.append(
+                Relations(
+                    path=doc.path,
+                    page_id=doc.page_id,
+                    source_id=doc.parent,
+                    target_id=doc.id,
+                    relation=rel
+                )
+            )
+        else:
+            for i, j in enumerate(config["startswith"]):
+                if col.startswith(i):
+                    relations.append(
+                        Relations(
+                            path=doc.path,
+                            page_id=doc.page_id,
+                            source_id=doc.parent,
+                            target_id=doc.id,
+                            relation=j
+                        )
+                    )
+                    break
+    return relations
+        
 def parse_html(html: str, path: str, page_id: int, root_tag: str = "html") -> list[Document]:
     soup = BeautifulSoup(html, "html.parser")
 
